0f80f0832eea51a5d1c7b2153892f1f65b8ad775
Asima <asimaislam1@gmail.com>
2022-03-09 16:43:25 +0000

    3

--- script.ts
+++ script.ts
@@ -253,6 +253,3 @@ drawGame()
 // //keep track of how many apples are eaten and display score
 // // restart game and snake
 
-// classic Nokia snake game that I made to understand basic game development. Followed a tutorial, but implemented some aspects myself (such as css, setting up HTML canvas, making the "snake" be able to come from opposite side when touching a wall etc).
-
-// Sound effect obtained from https://www.zapsplat.com
